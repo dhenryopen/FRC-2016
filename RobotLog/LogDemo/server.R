fd566693fec3aaa174a5041a91b193c8ec0c8d67
dhenryopen <ds_henry@yahoo.com>
2015-12-21 21:07:03 -0800

    Stable set for 12/22 demo

--- RobotLog/LogDemo/server.R
+++ RobotLog/LogDemo/server.R
@@ -24,30 +24,41 @@ robotlog[,'ram_free'] <- as.numeric(robotlog$"RAM Free")
 
 shinyServer(function(input, output) {
     
-    output$metricPlot <- renderPlot(function() {
+    output$metricPlot <- renderPlot({
 
+      # setup the plot based on the input variable
+      
       if(input$variable == "pct_bus_utilization"){
         plotData <- data.frame(var1 = robotlog$datetime, var2 = robotlog$pct_bus_utilization, Mode = robotlog$Mode)
-        ylabel = "Bus Utilization (%)"
+        ylabel = "Bus Utilization (%)\n"
       } else if(input$variable == "pct_cpu_low"){
         plotData <- data.frame(var1 = robotlog$datetime, var2 = robotlog$pct_cpu_low, Mode = robotlog$Mode)
-        ylabel = "CPU Low (%)"      
+        ylabel = "CPU Low (%)\n"      
       } else if(input$variable == "pct_cpu_time_critical"){
         plotData <- data.frame(var1 = robotlog$datetime, var2 = robotlog$pct_cpu_time_critical, Mode = robotlog$Mode)
-        ylabel = "CPU Time Critical (%)"
+        ylabel = "CPU Time Critical (%)\n"
       } else {
         plotData <- data.frame(var1 = robotlog$datetime, var2 = robotlog$ram_free, Mode = robotlog$Mode)
-        ylabel = "Free RAM"      
+        ylabel = "Free RAM\n"      
       }
+      
+      # trim plotData by the input slider parameters
+      
+      num_obs <- nrow(plotData)  # should correspond to nrecords variable in ui.R
+      lower_bound <- input$slider[1]
+      upper_bound <- input$slider[2]
+      plotData <- plotData[lower_bound:upper_bound,]
 
+      # plot the data
+      
       p <- ggplot(plotData, aes(var1, var2, colour=Mode)) + 
           geom_line(size = 1) +
           xlab("Time (ms)") + 
           ylab(ylabel) +
           theme_bw() +
-          scale_x_datetime(breaks=date_breaks("10 sec")) + 
+          scale_x_datetime(breaks=date_breaks("15 sec")) + 
           stat_smooth(method="loess") 
+    
       print(p)
       })
 })
-
--- RobotLog/LogDemo/ui.R
+++ RobotLog/LogDemo/ui.R
@@ -1,21 +1,30 @@
 library(shiny)
 
+# Need to tie this variable to an API call:
+
+nrecords = as.integer(597)
+
 shinyUI(pageWithSidebar(
-    
+
     # Application title
-    headerPanel("RoboRio Internal Metrics"),
+  
+    headerPanel("Robot Log Metrics"),
     
     # Sidebar with controls to select the variable to plot against time
-    # and to specify whether outliers should be included
+
     sidebarPanel(
         selectInput("variable", "Metric:",
                     list("Bus Utilization (%)" = "pct_bus_utilization", 
                          "CPU Low (%)" = "pct_cpu_low", 
                          "CPU Time Critical (%)" = "pct_cpu_time_critical", 
-                         "Free RAM" = "ram_free"))
+                         "Free RAM" = "ram_free")),
+   
+        sliderInput("slider", label = "Record Range:", min = 1, 
+                    max = nrecords, value = c(1,nrecords))    
     ),
     
     # Show the caption and plot of the requested variable against time
+    
     mainPanel(
         h3(textOutput("caption")),
         plotOutput("metricPlot")
