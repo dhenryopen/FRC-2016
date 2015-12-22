c35fec9a5faa27ce0f0711af68f3b77fb24da28e
Dave Henry <ds_henry@yahoo.com>
2015-12-22 07:48:23 -0800

    Added download option

--- RobotLog/LogDemo/server.R
+++ RobotLog/LogDemo/server.R
@@ -7,40 +7,66 @@ library(scales)
 shinyServer(function(input, output) {
     
     output$metricPlot <- renderPlot({
-
-      # setup the plot based on the input variable
-      
-      if(input$variable == "pct_bus_utilization"){
-        plotData <- data.frame(var1 = robotlog$datetime, var2 = robotlog$pct_bus_utilization, Mode = robotlog$Mode)
-        ylabel = "Bus Utilization (%)\n"
-      } else if(input$variable == "pct_cpu_low"){
-        plotData <- data.frame(var1 = robotlog$datetime, var2 = robotlog$pct_cpu_low, Mode = robotlog$Mode)
-        ylabel = "CPU Low (%)\n"      
-      } else if(input$variable == "pct_cpu_time_critical"){
-        plotData <- data.frame(var1 = robotlog$datetime, var2 = robotlog$pct_cpu_time_critical, Mode = robotlog$Mode)
-        ylabel = "CPU Time Critical (%)\n"
-      } else {
-        plotData <- data.frame(var1 = robotlog$datetime, var2 = robotlog$ram_free, Mode = robotlog$Mode)
-        ylabel = "Free RAM\n"      
-      }
-      
-      # trim plotData by the input slider parameters
-      
-      num_obs <- nrow(plotData)  # should correspond to nrecords variable in ui.R
-      lower_bound <- input$slider[1]
-      upper_bound <- input$slider[2]
-      plotData <- plotData[lower_bound:upper_bound,]
-
-      # plot the data
-      
-      p <- ggplot(plotData, aes(var1, var2, colour=Mode)) + 
-          geom_line(size = 1) +
-          xlab("Time (ms)") + 
-          ylab(ylabel) +
-          theme_bw() +
-          scale_x_datetime(breaks=date_breaks("15 sec")) + 
-          stat_smooth(method="loess") 
+        
+        # setup the plot based on the input variable
+        
+        if(input$variable == "pct_bus_utilization"){
+            plotData <- data.frame(var1 = robotlog$datetime, var2 = robotlog$pct_bus_utilization, Mode = robotlog$Mode)
+            ylabel = "Bus Utilization (%)\n"
+        } else if(input$variable == "pct_cpu_low"){
+            plotData <- data.frame(var1 = robotlog$datetime, var2 = robotlog$pct_cpu_low, Mode = robotlog$Mode)
+            ylabel = "CPU Low (%)\n"      
+        } else if(input$variable == "pct_cpu_time_critical"){
+            plotData <- data.frame(var1 = robotlog$datetime, var2 = robotlog$pct_cpu_time_critical, Mode = robotlog$Mode)
+            ylabel = "CPU Time Critical (%)\n"
+        } else {
+            plotData <- data.frame(var1 = robotlog$datetime, var2 = robotlog$ram_free, Mode = robotlog$Mode)
+            ylabel = "Free RAM\n"      
+        }
+        
+        # trim plotData by the input slider parameters
+        
+        num_obs <- nrow(plotData)  # should correspond to nrecords variable in ui.R
+        lower_bound <- input$slider[1]
+        upper_bound <- input$slider[2]
+        plotData <- plotData[lower_bound:upper_bound,]
+        
+        # plot the data
+        
+        p <- ggplot(plotData, aes(var1, var2, colour=Mode)) + 
+            geom_line(size = 1) +
+            xlab("Time (ms)") + 
+            ylab(ylabel) +
+            theme_bw() +
+            scale_x_datetime(breaks=date_breaks("15 sec")) + 
+            stat_smooth(method="loess") 
+        
+        print(p)
+    })
     
-      print(p)
-      })
+    # Render the table on the page
+    
+    output$tableData = renderDataTable({
+        lower_bound <- input$slider[1]
+        upper_bound <- input$slider[2]
+        tableData <- robotlog[lower_bound:upper_bound,c("datetime","pct_bus_utilization","pct_cpu_low","pct_cpu_time_critical","ram_free")]
+        colnames(tableData) <- c("Date / Time","Bus Utilization (%)","CPU Low (%)", "CPU Time Critical (%)","Free Ram")
+        options(digits.secs=3)
+        tableData
+    })
+    
+    # Download the table (full data set bounded by slider)
+    
+    output$downloadData <- downloadHandler(
+        filename = function() { 
+           paste('robotlog', '.csv', sep='') },
+        content = function(file) {
+            lower_bound <- input$slider[1]
+            upper_bound <- input$slider[2]
+            tableData <- robotlog[lower_bound:upper_bound,c("Date","Time","FMS Present","E-Stop","Team Station","Countdown","Code Start","Brownout","Mode","Code?","Contoller Type","Error Strings","Robot IP","DB Mode","Event Name","Event Type","Event Number","Event Replay","Camera IP","Disk Free","RM Block","RAM Free","CPU % ISR","CPU % Time Crit","CPU % Time Str","CPU % High","CPU % Above","CPU % Normal","CPU % Low","% Bus Utilization","Bus off Cnt","TX_FIFO Full Count","Receive Error Cnt","Transmit Error Cnt","HID Outputs","Rumble Low","Rumble High")
+]
+            write.csv(tableData, file, row.names = FALSE)
+        }
+    )
 })
+
--- RobotLog/LogDemo/ui.R
+++ RobotLog/LogDemo/ui.R
@@ -3,28 +3,32 @@ library(shiny)
 nrecords = nrow(robotlog)  # upper bound for slider input control
 
 shinyUI(pageWithSidebar(
-
+    
     # Application title
-  
+    
     headerPanel("Robot Log Metrics"),
     
     # Sidebar with controls to select the variable to plot against time
-
+    
     sidebarPanel(
         selectInput("variable", "Metric:",
                     list("Bus Utilization (%)" = "pct_bus_utilization", 
                          "CPU Low (%)" = "pct_cpu_low", 
                          "CPU Time Critical (%)" = "pct_cpu_time_critical", 
                          "Free RAM" = "ram_free")),
-   
+        
         sliderInput("slider", label = "Record Range:", min = 1, 
-                    max = nrecords, value = c(1,nrecords))    
+                    max = nrecords, value = c(1,nrecords)),
+        
+        downloadButton('downloadData', 'Download')
     ),
     
     # Show the caption and plot of the requested variable against time
     
     mainPanel(
         h3(textOutput("caption")),
-        plotOutput("metricPlot")
+        plotOutput("metricPlot"),
+        tabPanel('metricTable',
+                 dataTableOutput("tableData"))
     )
 ))
